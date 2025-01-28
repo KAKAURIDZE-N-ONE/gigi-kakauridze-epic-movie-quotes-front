5bc05c11ecfd13176e942b632fe616b4b9efceed
KAKAURIDZE-N-ONE <g_kakauridze3@cu.edu.ge>
2025-01-28 18:36:46 +0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Gela barkalaia</div>;
+}
---
+++ pages/personal.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const personal: React.FC = () => {
+  return <div>sdadasd</div>;
+};
+
+export default personal;
